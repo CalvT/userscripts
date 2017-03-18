77eeb53e79fb972fa0cedcd71a4e8c2984da7626
angussidney <angussidney@users.noreply.github.com>
2017-03-18 12:11:10 +1100

    Create autopull.user.js

---
+++ autopull/autopull.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Autopull checkbox
+// @namespace   https://github.com/Charcoal-SE
+// @description Adds a checkbox to the SmokeDetector repo to toggle autopull
+// @include     *://github.com/*/SmokeDetector/edit/*
+// @include     *://github.com/*/SmokeDetector/pull/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
